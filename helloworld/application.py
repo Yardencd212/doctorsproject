a7635d007b1fd0454545087af92c4ea63a9999cf
EC2 Default User <ec2-user@ip-172-31-90-155.ec2.internal>
2021-08-13 19:20:08 +0000

    setups

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,9 +1,13 @@
 #!flask/bin/python
 import json
-from flask import Flask, Response
+from flask import Flask, Response, request
 from helloworld.flaskrun import flaskrun
+import requests
+import boto3
+from flask_cors import CORS
 
 application = Flask(__name__)
+CORS(application, resources={r"/*": {"origins": "*"}})
 
 @application.route('/', methods=['GET'])
 def get():
