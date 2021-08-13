ab0c59213084777fa8ff2b0df666bb2c9263861f
EC2 Default User <ec2-user@ip-172-31-90-155.ec2.internal>
2021-08-13 19:25:59 +0000

    added set and get doctors

--- helloworld/application.py
+++ helloworld/application.py
@@ -17,5 +17,40 @@ def get():
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
+@application.route('/get_doctors', methods=['GET'])
+def get_id():
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('doctors')
+    # replace table scan ###
+    resp = table.scan()
+    print(str(resp))
+    return Response(json.dumps(str(resp['Items'])), mimetype='application/json', status=200)
+    
+# curl -i -X POST -d'{"name":"Shiran", "department":"Surgery", "years":"20"}' -H "Content-Type: application/json" http://localhost:5000/set_doctor/4
+@application.route('/set_doctor/<id>', methods=['POST'])
+def set_doc(id):
+    
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('doctors')
+    # get post data  
+    data = request.data
+    # convert the json to dictionary
+    data_dict = json.loads(data)
+    # retreive the parameters
+    name = data_dict.get('name','default')
+    department = data_dict.get('department','defualt')
+    years = data_dict.get('years', 'default')
+
+    item={
+    'id': id,
+    'name': name,
+    'department': department, 
+    'years': years 
+     }
+    table.put_item(Item=item)
+    
+    return Response(json.dumps(item), mimetype='application/json', status=200)
+
+
 if __name__ == '__main__':
     flaskrun(application)
